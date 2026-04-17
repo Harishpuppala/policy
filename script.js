8e9d11a2ac07768ecb56414ff3a16a122fc6a2ad
Harish Puppala <38903506+Harishpuppala@users.noreply.github.com>
2026-04-17 20:42:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,12 @@
-let colleges = [];
-let templateText = "";
-
-fetch("template.txt")
-.then(res => res.text())
-.then(data => {
-templateText = data;
-});
-
-fetch("colleges.json")
-.then(res => res.json())
-.then(data => {
-colleges = data;
-populateDropdown(colleges);
-});
-
-function populateDropdown(list){
-
-let dropdown = document.getElementById("collegeDropdown");
-dropdown.innerHTML = "";
-
-list.forEach((college,index)=>{
-
-let option = document.createElement("option");
-option.value = index;
-option.text = college.name;
-
-dropdown.appendChild(option);
-
-});
-
-}
-
-function filterColleges(){
-
-let search = document.getElementById("searchBox").value.toLowerCase();
-
-let filtered = colleges.filter(c =>
-c.name.toLowerCase().includes(search)
-);
-
-populateDropdown(filtered);
-
-}
-
 function downloadPDF(){
 
 let dropdown = document.getElementById("collegeDropdown");
 
-if(dropdown.selectedIndex === -1){
-alert("Please select a university");
-return;
-}
+let index = dropdown.value;
 
-let selectedName = dropdown.options[dropdown.selectedIndex].text;
+let college = colleges[index];
 
-let college = colleges.find(c => c.name === selectedName);
-
-let policy = templateText
+let policy = template
 .replaceAll("{{UNIVERSITY_NAME}}", college.name)
 .replaceAll("{{AIRSPACE_ZONE}}", college.zone);
 
@@ -64,9 +14,29 @@ const { jsPDF } = window.jspdf;
 
 let doc = new jsPDF();
 
-let lines = doc.splitTextToSize(policy,180);
+let pageHeight = doc.internal.pageSize.height;
 
-doc.text(lines,10,10);
+let margin = 10;
+
+let y = margin;
+
+let lines = doc.splitTextToSize(policy, 180);
+
+lines.forEach(line => {
+
+if (y > pageHeight - margin) {
+
+doc.addPage();
+
+y = margin;
+
+}
+
+doc.text(line, margin, y);
+
+y += 7;
+
+});
 
 doc.save("Drone_Policy.pdf");
 
